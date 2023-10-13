89b99f7e78b313282e67d81ce8c5b22764f4518d
ripeplantain <irvingmanny@gmail.com>
2023-10-13 11:19:05 +0000

    dom pdf package added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,36 +26,36 @@ class HomeController extends Controller
     {
         $awards = Award::with('student', 'department')->latest();
 
-        if (request('search')){
+        if (request('search')) {
             $awards
-                ->where('congregation','like','%'.request('search').'%')
-                ->orWhereHas('department',function($query){
-                    $query->where('short_name','like','%'.request('search').'%');    
+                ->where('congregation', 'like', '%' . request('search') . '%')
+                ->orWhereHas('department', function ($query) {
+                    $query->where('short_name', 'like', '%' . request('search') . '%');
                 })
-                ->orWhereHas('department',function($query){
-                    $query->where('long_name','like','%'.request('search').'%');    
+                ->orWhereHas('department', function ($query) {
+                    $query->where('long_name', 'like', '%' . request('search') . '%');
                 })
-                ->orWhere('year','like','%'.request('search').'%')
-                ->orWhere('sponsor','like','%'.request('search').'%')
-                ->orWhere('award_name','like','%'.request('search').'%');
+                ->orWhere('year', 'like', '%' . request('search') . '%')
+                ->orWhere('sponsor', 'like', '%' . request('search') . '%')
+                ->orWhere('award_name', 'like', '%' . request('search') . '%');
         }
 
-        if(request()->user()->can('admin')){
+        if (request()->user()->can('admin')) {
             $awards;
-        } else if(request()->user()->can('college')){
-            $awards->whereHas('department',function($query){
-                $query->whereHas('faculty',function($sql){
-                    $sql->whereHas('college',function($relation){
-                        $relation->where('collegeid',auth()->user()->department->faculty->college->collegeid);
+        } else if (request()->user()->can('college')) {
+            $awards->whereHas('department', function ($query) {
+                $query->whereHas('faculty', function ($sql) {
+                    $sql->whereHas('college', function ($relation) {
+                        $relation->where('collegeid', auth()->user()->department->faculty->college->collegeid);
                     });
                 });
             });
-        }else {
+        } else {
 
-            $awards->where('deptid',auth()->user()->deptid);
+            $awards->where('deptid', auth()->user()->deptid);
         }
 
-        return view('home',[
+        return view('home', [
             'awards' => $awards->paginate(10),
         ]);
     }
@@ -64,45 +64,45 @@ class HomeController extends Controller
     public function filter(Request $request)
     {
         $param = $request->route()->parameters();
-        
-        $instance = Award::latest()->where('session',$param);
 
-  
+        $instance = Award::latest()->where('session', $param);
 
-        if($instance->count() ==  0){
+
+
+        if ($instance->count() ==  0) {
             $instance = Award::latest();
         }
 
-        if (request('search')){
+        if (request('search')) {
             $instance
-                ->where('congregation','like','%'.request('search').'%')
-                ->orWhereHas('department',function($query){
-                    $query->where('short_name','like','%'.request('search').'%');    
+                ->where('congregation', 'like', '%' . request('search') . '%')
+                ->orWhereHas('department', function ($query) {
+                    $query->where('short_name', 'like', '%' . request('search') . '%');
                 })
-                ->orWhereHas('department',function($query){
-                    $query->where('long_name','like','%'.request('search').'%');    
+                ->orWhereHas('department', function ($query) {
+                    $query->where('long_name', 'like', '%' . request('search') . '%');
                 })
-                ->orWhere('year','like','%'.request('search').'%')
-                ->orWhere('sponsor','like','%'.request('search').'%')
-                ->orWhere('award_name','like','%'.request('search').'%');
+                ->orWhere('year', 'like', '%' . request('search') . '%')
+                ->orWhere('sponsor', 'like', '%' . request('search') . '%')
+                ->orWhere('award_name', 'like', '%' . request('search') . '%');
         }
 
-        if(request()->user()->can('admin')){
+        if (request()->user()->can('admin')) {
             $instance;
-        } else if(request()->user()->can('college')){
-            $instance->whereHas('department',function($query){
-                $query->whereHas('faculty',function($sql){
-                    $sql->whereHas('college',function($relation){
-                        $relation->where('collegeid',auth()->user()->department->faculty->college->collegeid);
+        } else if (request()->user()->can('college')) {
+            $instance->whereHas('department', function ($query) {
+                $query->whereHas('faculty', function ($sql) {
+                    $sql->whereHas('college', function ($relation) {
+                        $relation->where('collegeid', auth()->user()->department->faculty->college->collegeid);
                     });
                 });
             });
-        }else {
+        } else {
 
-            $instance->where('deptid',auth()->user()->deptid);
+            $instance->where('deptid', auth()->user()->deptid);
         }
 
-        return view('home',[
+        return view('home', [
             'awards' => $instance->paginate(10),
         ]);
     }
